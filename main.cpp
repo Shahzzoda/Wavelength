c12d134a657705f7ba668cbfd0059ba864ac7760
Shahzoda D <shahzoda01@gmail.com>
2017-03-15 23:11:24 -0400

    Formula fixed.

--- main.cpp
+++ main.cpp
@@ -6,27 +6,31 @@ using namespace std;
 
 int main()
 {
-    int ground = 1; //groundlvl orbital in each series
+    int excited = 1; //groundlvl orbital in each series
     float wavelength = 0; //calculated wavelength (cm)
     float wavelengthNm = 0; //wavelength in nanometers
 
     const float R = 109677.581; //constant that will be used in formula
 
 
-    for(int excited=1; excited <= 6; excited++)
+    for(int ground=1; ground <= 6; ground++)
     {
-        cout << "title" << endl;
+
+        cout << "title";
         for(int i = 1; i <= 10; i++)
         {
-            cout << ground << endl;
-            ground += 1;
-
+            //cout << endl << ground << endl << excited << endl;
+            cout << endl << endl << ground << endl << excited;
+            excited += 1;
             wavelength = 1/(R*(pow(ground, -2)-pow(excited,-2)));
             wavelengthNm = wavelength * pow(10,7);
-            cout << wavelengthNm;
+            cout << endl << wavelengthNm;
+
+            //cout << endl << ground << endl << excited << endl;
         }
         cout << "\nhello" << endl << endl;
-        ground -= 9;
+        excited -= 9;
+        //cout << endl << ground << endl << excited << endl;
     }
 
     return 0;
