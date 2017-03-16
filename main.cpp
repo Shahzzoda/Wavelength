7e59b0492e1630436e1d759ec26d1c980584376a
Shahzoda D <shahzoda01@gmail.com>
2017-03-15 23:41:38 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,27 +11,36 @@ int main()
     float wavelengthNm = 0; //wavelength in nanometers
 
     const float R = 109677.581; //constant that will be used in formula
-
+    string seriesTitle (int);
 
     for(int ground=1; ground <= 6; ground++)
     {
-
-        cout << "title";
+        cout << "\t" << seriesTitle(ground) << " SERIES" << endl << "\t    (n= "<< ground << ")" << endl;
+        cout << "Excited:\t\t" << "Wavelength:";
         for(int i = 1; i <= 10; i++)
         {
-            //cout << endl << ground << endl << excited << endl;
-            cout << endl << endl << ground << endl << excited;
             excited += 1;
             wavelength = 1/(R*(pow(ground, -2)-pow(excited,-2)));
             wavelengthNm = wavelength * pow(10,7);
-            cout << endl << wavelengthNm;
-
-            //cout << endl << ground << endl << excited << endl;
+            cout << endl << excited << "\t\t\t" << wavelengthNm;
         }
-        cout << "\nhello" << endl << endl;
         excited -= 9;
-        //cout << endl << ground << endl << excited << endl;
+        cout << endl << endl << endl;
     }
 
     return 0;
 }
+
+string seriesTitle (int ground) {
+if (ground == 1)
+    return "LEYMAN";
+else if (ground == 2)
+    return "BALMER";
+else if (ground == 3)
+    return "PASCHEN";
+else if (ground == 4)
+    return "BRACKET";
+else if (ground == 5)
+    return "PFUND";
+else if (ground == 6)
+    return "HUMPHREY";}
